935c05b11a7fb500f41dcc8d6af3fcaecd7b5b4b
Gabriel Nogueria Schultz <gabrielschultz507@gmail.com>
2026-08-11 08:19:14 -0300

    após correção do marcio

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const h1 = document.querySelector("h1")
 const botao = document.querySelector("button")
 const fundo = document.querySelector("body")
 
 botao.addEventListener("click", mudarCor)
 
 function mudarCor() {
-    fundo.classList.add("mudar-fundo")
-    h1.classList.add("mudar-h1")
+    fundo.classList.toggle("mudar-fundo")
 }
